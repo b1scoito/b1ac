ea9adf3a6247faf36d164467856e219a4bd4e801
Rafael Felix <rafaelbadas123@gmail.com>
2018-10-26 05:43:39 -0300

    Tirei os bagulho de renomear q tava bugano o self destruct

--- b1ac/b1ac/Program.cs
+++ b1ac/b1ac/Program.cs
@@ -8,20 +8,12 @@ namespace b1ac
 {
     internal static class Program
     {
-        public static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
-        }
         [STAThread]
         private static void Main()
         {
-            File.Move(AppDomain.CurrentDomain.FriendlyName, RandomString(5) + ".exe");
-            Thread.Sleep(500);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new mainform());
         }
-        private static Random random = new Random();
     }
 }
